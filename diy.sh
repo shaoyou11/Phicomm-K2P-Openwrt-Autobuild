71b98c7f5509947ff09127088f5cf8cff1fc0194
shaoyou11 <shaobinyx@gmail.com>
2020-08-03 19:37:40 +0800

    Add files via upload

--- diy.sh
+++ diy.sh
@@ -7,3 +7,25 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+# Uncomment a feed source
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# Modify Default Theme
+sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase=/luci-static/argon' package/lean/default-settings/files/zzz-default-settings
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+
+# Update Luci theme argon  
+rm -rf package/lean/luci-theme-argon  
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+
+git clone https://github.com/yangsongli/luci-theme-atmaterial package/luci-theme-atmaterial
+git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
+git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
+
+mkdir -p package/lean/smartdns && wget -P package/lean/smartdns https://raw.githubusercontent.com/openwrt/packages/master/net/smartdns/Makefile
+git clone https://github.com/shaoyou11/luci-app-smartdns.git package/lean/luci-app-smartdns
